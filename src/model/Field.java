9b8abb75dd9685807d00d67e96b1325254535bcc
Super <superkolobok@gmail.com>
2015-08-20 19:57:24 +0300

    Field - created

---
+++ src/model/Field.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.*;
+
+public class Field {
+
+    public static final int FIELD_SIZE = 3;
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public static int getFieldSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure (final Point point) {
+        return field[point.x][point.y];
+    }
+
+    public void setFigure (final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
+
+
+}
