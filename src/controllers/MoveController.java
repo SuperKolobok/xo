ef1736cb573688e81a40b98c5d9fb9689032a0cd
Super <superkolobok@gmail.com>
2015-08-21 12:06:56 +0300

    MoveController - created

---
+++ src/controllers/MoveController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import model.Field;
+import model.Figure;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure (final Field field, final Figure figure, final Point point){
+
+    }
+
+}
